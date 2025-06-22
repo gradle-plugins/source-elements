548b0b22e65ed0bd0eb7cbb5daeae4c955a24b66
Daniel Lacasse <daniel@nokee.dev>
2025-06-22 15:17:28 +0200

    Remove Element inheritance
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- elements/src/main/java/dev/nokee/elements/core/WorkspaceElement.java
+++ elements/src/main/java/dev/nokee/elements/core/WorkspaceElement.java
@@ -1,11 +1,9 @@
 package dev.nokee.elements.core;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
-// TODO: Remove extends from Element
-public abstract class WorkspaceElement extends Element {
+public abstract class WorkspaceElement {
 	private final Object identifier;
 
 	protected WorkspaceElement() {
@@ -17,9 +15,4 @@ public abstract class WorkspaceElement extends Element {
 	}
 
 	public abstract List<ProjectElement> getProjects();
-
-	@Override
-	public void accept(Visitor visitor) {
-		visitor.visit(this);
-	}
 }
